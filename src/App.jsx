7fb78bce63129248446dbc7cb6c4308c1a698086
CoderGirl <angi.adema@gmail.com>
2023-09-28 18:11:36 -0600

    updated App.jsx with practice JSX code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return "Hello World!";
+  return (
+    <div className="large" id="largeDiv">
+      <label htmlFor="inputId">Good Job!</label>
+      <input id="inputId" type="number" value={3} />
+    </div>
+  );
 }
 
 export default App;
