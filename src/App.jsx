02560be212082386483c09344fdd5c62c020355c
CoderGirl <angi.adema@gmail.com>
2023-09-29 18:08:55 -0600

    added the Name.jsx file with sample name component code as well as imported it into App.jsx in order to render the name

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { Name } from "./Name";
+
 function App() {
   return (
-    <div className="large" id="largeDiv">
-      <label htmlFor="inputId">Good Job!</label>
-      <input id="inputId" type="number" value={3} />
+    <div>
+      <Name />
     </div>
   );
 }
---
+++ src/Name.jsx
@@ -0,0 +1,3 @@
+export function Name() {
+  return <h1>Angi Adema</h1>;
+}
