00fac6222d4fba810e0a02c57856c97a921fc8f0
CoderGirl <angi.adema@gmail.com>
2023-09-30 15:24:03 -0600

    added the strict mode back into main.jsx and updated the comments

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,10 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
+
+// Removed the React.StrictMode for now so that it is not logging results twice in the dev tools. Just uncomment to add them back in.
