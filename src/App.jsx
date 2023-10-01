e559b31eddbd8160c1103e0522b23a5be000cf7e
CoderGirl <angi.adema@gmail.com>
2023-10-01 15:00:42 -0600

    worked on practice files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+// import React from "react";
+import { User } from "./User";
 // import { NameClass } from "./NameClass";
 // import { NameFunc } from "./NameFunc";
-import { TodoListItem } from "./TodoListItem";
+// import { TodoListItem } from "./TodoListItem";
+
+// function App() {
+//   return <TodoListItem isComplete>First List Item</TodoListItem>;
+// }
 
 function App() {
-  return <TodoListItem isComplete>First List Item</TodoListItem>;
+  return <User name="Angi" />;
 }
 
 export default App;
