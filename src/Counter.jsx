c4b31fe35fdc7c31fb124cc1225ce0f61e5fa6d5
CoderGirl <angi.adema@gmail.com>
2023-10-02 11:21:18 -0600

    updated code in Counter.jsx to clarify what I was originally trying to do

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,17 +3,18 @@ import { useState } from "react";
 export function Counter() {
   const [counter, setCounter] = useState(0);
 
-  //   function handleClick() {
-  //     setCounter(counter + 1);
-  //   }
+  function handleClick() {
+    setCounter((currentCount) => currentCount + 1);
+  }
 
   //   setCounter((currentCount) => {
   //     return currentCount + 1;
   //   });
 
   return (
-    <h1 onClick={() => setCounter((currentCount) => currentCount + 1)}>
-      Increment: {counter}
-    </h1>
+    <h1 onClick={handleClick}>Increment: {counter}</h1>
+    // <h1 onClick={() => setCounter((currentCount) => currentCount + 1)}>
+    //   Increment: {counter}
+    // </h1>
   );
 }
