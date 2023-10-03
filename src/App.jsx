897fccbf7a3ca58601d9d03bd24d8f42dbfa7c07
CoderGirl <angi.adema@gmail.com>
2023-10-02 19:27:45 -0600

    commented out Counter.jsx and completed project 20 in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,94 @@
 // import { NameClass } from "./NameClass";
 // import { NameFunc } from "./NameFunc";
 // import { TodoListItem } from "./TodoListItem";
-// import { useState } from "react";
-import { Counter } from "./Counter";
+import { useState } from "react";
+// import { Counter } from "./Counter";
 
-// function App() {
-//   return <TodoListItem isComplete>First List Item</TodoListItem>;
-// }
+const INITIAL_ARRAY = ["A", "B", "C"];
 
 function App() {
-  return <Counter />;
+  const [array, setArray] = useState(["A", "B", "C"]);
+  const [value, setValue] = useState("");
+
+  function removeFirst() {
+    setArray((existingArray) => {
+      return existingArray.slice(1);
+    });
+  }
+
+  function removeLetter() {
+    setArray((existingArray) => {
+      return existingArray.filter((letter) => letter !== "B");
+    });
+  }
+
+  function addLetterToStart() {
+    setArray((existingArray) => {
+      return ["P", ...existingArray];
+    });
+  }
+
+  function addLetterToEnd() {
+    setArray((existingArray) => {
+      return [...existingArray, "Z"];
+    });
+  }
+
+  function clearArray() {
+    setArray([]);
+  }
+
+  function resetArray() {
+    setArray(INITIAL_ARRAY);
+  }
+
+  function updateAtoN() {
+    setArray((existingArray) => {
+      return existingArray.map((letter) => {
+        if (letter === "A") {
+          return "N";
+        } else {
+          return letter;
+        }
+      });
+    });
+  }
+
+  function addInputValue() {
+    // const inputValue = prompt("Enter a letter");
+    <input type="text" />;
+    setArray((existingArray) => {
+      return [value, ...existingArray];
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={removeFirst}>Remove First Index</button>
+      <br />
+      <button onClick={removeLetter}>Remove B From Array</button>
+      <br />
+      <button onClick={addLetterToStart}>Add Letter to Start of Array</button>
+      <br />
+      <button onClick={addLetterToEnd}>Add Letter to End of Array</button>
+      <br />
+      <button onClick={clearArray}>Clear the Array</button>
+      <br />
+      <button onClick={resetArray}>Set Array to Initial Value</button>
+      <br />
+      <button onClick={updateAtoN}>Update All As to Ns</button>
+      <br />
+      <br />
+      <input value={value} onChange={(event) => setValue(event.target.value)} />
+      <br />
+      <button onClick={() => addInputValue(value)}>
+        Add Input to Start of Array
+      </button>
+      <br />
+      <br />
+      {array.join(", ")}
+    </div>
+  );
 }
 
 export default App;
-
-// return <TodoListItem isComplete>List Item 1</TodoListItem>;
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export function Counter() {
-  const [counter, setCounter] = useState(0);
+// export function Counter() {
+//   const [counter, setCounter] = useState(0);
 
-  function handleClick() {
-    setCounter((currentCount) => currentCount + 1);
-  }
+//   function handleClick() {
+//     setCounter((currentCount) => currentCount + 1);
+//   }
 
-  //   setCounter((currentCount) => {
-  //     return currentCount + 1;
-  //   });
+//   setCounter((currentCount) => {
+//     return currentCount + 1;
+//   });
 
-  return (
-    <h1 onClick={handleClick}>Increment: {counter}</h1>
-    // <h1 onClick={() => setCounter((currentCount) => currentCount + 1)}>
-    //   Increment: {counter}
-    // </h1>
-  );
-}
+// return (
+//   <h1 onClick={handleClick}>Increment: {counter}</h1>
+// <h1 onClick={() => setCounter((currentCount) => currentCount + 1)}>
+//   Increment: {counter}
+// </h1>
+//   );
+// }
