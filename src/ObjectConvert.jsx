45251eb66ad5b17b5e88f16667c727548bdc1940
CoderGirl <angi.adema@gmail.com>
2023-10-10 11:46:59 -0600

    added the ObjectConvert.jsx file to clear out the App.jsx for additional practice concepts

---
+++ src/ObjectConvert.jsx
@@ -0,0 +1,35 @@
+// import { useState } from "react";
+
+// function App() {
+//   const [items, setItems] = useState([
+//     { id: crypto.randomUUID(), name: "Item 1" },
+//     { id: crypto.randomUUID(), name: "Item 2" },
+//   ]);
+
+//   function addItem() {
+//     setItems((currentItems) => {
+//       return [...currentItems, { id: crypto.randomUUID(), name: "New Item" }];
+//     });
+//   }
+
+//   return (
+//     <>
+//       <button onClick={addItem}>Add Item</button>
+{
+  /* .map() is converting the object into an array that React can use. Also the key has to be set inside the div tag and not inside the div itself as shown below.*/
+}
+//       <pre>
+//         {items.map((item) => {
+//           return (
+//             <div key={item.id}>
+//               {item.name}
+//               <input type="text" />
+//             </div>
+//           );
+//         })}
+//       </pre>
+//     </>
+//   );
+// }
+
+// export default App;
