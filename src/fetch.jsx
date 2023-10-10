7e175cf062edd59cd15c704409de4396a99efd9d
CoderGirl <angi.adema@gmail.com>
2023-10-10 11:43:58 -0600

    commented out the Fetch.jsx file in order to keep practicing React concept

--- src/fetch.jsx
+++ src/fetch.jsx
@@ -1,6 +1,6 @@
 // import { useEffect, useState } from "react";
 
-// export default function App() {
+// export function App() {
 //   const [users, setUsers] = useState();
 //   const [loading, setLoading] = useState(true);
 //   const [error, setError] = useState();
@@ -8,8 +8,8 @@
 //   useEffect(() => {
 //     setLoading(true);
 //     setError(undefined);
-// setUsers(undefined); Use this to reset the users if you want to do this.
-//     fetch("https://jsonplaceholder.typicode.com/users")
+//     // setUsers(undefined); Use this to reset the users if you want to do this.
+//     fetch("https://jsonplaceholder.typicode.com/usersaaa")
 //       .then((res) => {
 //         if (res.status === 200) {
 //           return res.json();
