dd07648fb0831b77bdaa8fd7a2436587acddb157
CoderGirl <angi.adema@gmail.com>
2023-10-27 10:11:44 -0600

    useReducer practice

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,9 +1,22 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const ACTIONS = {
+  DECREMENT: "DECREMENT",
+  INCREMENT: "INCREMENT",
+  RESET: "RESET",
+  CHANGE_COUNT: "CHANGE_COUNT",
+};
 
 function reducer(count, action) {
-  switch (count.type) {
-    case "DECREMENT":
+  switch (action.type) {
+    case ACTIONS.DECREMENT:
       return count - 1;
+    case ACTIONS.INCREMENT:
+      return count + 1;
+    case ACTIONS.RESET:
+      return 0;
+    case ACTIONS.CHANGE_COUNT:
+      return count + action.payload.value;
     default:
       return count;
   }
@@ -14,9 +27,21 @@ export function Counter({ initialCount = 0 }) {
 
   return (
     <>
-      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DECREMENT })}>-</button>
       {count}
-      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <button onClick={() => dispatch({ type: ACTIONS.INCREMENT })}>+</button>
+      <br />
+      <br />
+      <button onClick={() => dispatch({ type: ACTIONS.RESET })}>Reset</button>
+      <br />
+      <br />
+      <button
+        onClick={() =>
+          dispatch({ type: ACTIONS.CHANGE_COUNT, payload: { value: 5 } })
+        }
+      >
+        +5
+      </button>
     </>
   );
 }
