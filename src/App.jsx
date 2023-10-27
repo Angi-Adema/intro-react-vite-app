aaf6537fafb079b9242f23d15b5ffc4194e94b87
CoderGirl <angi.adema@gmail.com>
2023-10-27 14:07:39 -0600

    updated the App.jsx file for useContext Hook demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,36 @@
-import { useState } from "react";
-import { useFetch } from "./useFetch";
+import { useEffect, useState } from "react";
 
-// If the API does not work, use these local URLs
-// const URLS = {
-// USERS: "users.json",
-// POSTS: "posts.json",
-// COMMENTS: "comments.json",
-// }
+export default function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-const URLS = {
-  USERS: "https://jsonplaceholder.typicode.com/users",
-  POSTS: "https://jsonplaceholder.typicode.com/posts",
-  COMMENTS: "https://jsonplaceholder.typicode.com/comments",
-};
+  function toggleTheme() {
+    setIsDarkMode((d) => !d);
+  }
 
-function App() {
-  const [url, setUrl] = useState(URLS.USERS);
-
-  const { data, isLoading, isError } = useFetch(url);
+  useEffect(() => {
+    document.body.style.background = isDarkMode ? "#333" : "white";
+    document.body.style.color = isDarkMode ? "white" : "#333";
+  }, [isDarkMode]);
 
   return (
     <>
-      <div>
-        <label>
-          <input
-            type="radio"
-            checked={url === URLS.USERS}
-            onChange={() => setUrl(URL.USERS)}
-          />
-          Users
-        </label>
-        <label>
-          <input
-            type="radio"
-            checked={url === URLS.POSTS}
-            onChange={() => setUrl(URLS.POSTS)}
-          />
-          Posts
-        </label>
-        <label>
-          <input
-            type="radio"
-            checked={url === URLS.COMMENTS}
-            onChange={() => setUrl(URLS.COMMENTS)}
-          />
-          Comments
-        </label>
-      </div>
-      {isLoading ? (
-        <h1>Loading...</h1>
-      ) : isError ? (
-        <h1>Error</h1>
-      ) : (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      )}
+      <button
+        style={{
+          background: isDarkMode ? "white" : "#333",
+          color: isDarkMode ? "#333" : "white",
+          border: "none",
+          padding: ".5em",
+          borderRadius: ".25em",
+          cursor: "pointer",
+        }}
+        onClick={toggleTheme}
+      >
+        Toggle Theme
+      </button>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+        quibusdam.
+      </p>
     </>
   );
 }
-
-export default App;
