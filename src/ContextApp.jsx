c9dac294c8698ae99c1bacbe59bab955e5b1655e
CoderGirl <angi.adema@gmail.com>
2023-10-30 12:30:53 -0600

    added ContextApp.jsx in order to save the App.jsx file for this demo

---
+++ src/ContextApp.jsx
@@ -0,0 +1,27 @@
+// import { useEffect, useState, createContext } from "react";
+// import { Child } from "./Child";
+
+// export const ThemeContext = createContext();
+
+// export default function App() {
+//   const [isDarkMode, setIsDarkMode] = useState(false);
+
+//   function toggleTheme() {
+//     setIsDarkMode((d) => !d);
+//   }
+
+//   useEffect(() => {
+//     document.body.style.background = isDarkMode ? "#333" : "white";
+//     document.body.style.color = isDarkMode ? "white" : "#333";
+//   }, [isDarkMode]);
+
+//   return (
+//     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+//       <Child />
+//       <p>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+//         quibusdam.
+//       </p>
+//     </ThemeContext.Provider>
+//   );
+// }
