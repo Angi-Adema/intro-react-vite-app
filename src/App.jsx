4ffa92dbda003041ba477c5e626b296f15a97c9a
CoderGirl <angi.adema@gmail.com>
2023-11-04 18:16:51 -0600

    added the pages folder to hold the demo of the route pages of Home.jsx, About.jsx and Store.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
+
 export default function App() {
-  return <h1>Hi!</h1>;
+  let component;
+
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break;
+    case "/about":
+      component = <About />;
+      break;
+    case "/store":
+      component = <Store />;
+      break;
+  }
+  return component;
 }
